44bc1192912ba60f0b61796d4707869254b18d1b
Ziyi Dai <ziyidai@Ziyis-MacBook-Pro.local>
2025-10-08 16:00:39 -0600

    test

--- assignment.sh
+++ assignment.sh
@@ -28,7 +28,7 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
-mkdir data
+mkdir ./data
 # 2. Move the ./rawdata directory to ./data/raw
 mv ./rawdata ./data/raw
 # 3. List the contents of the ./data/raw directory
@@ -42,10 +42,10 @@ mkdir user_logs
 mkdir event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 cd ..
-cp raw/*server*.log processed/server_logs
+cp raw/*server*.log processed/server_logs/
 # 6. Repeat the above step for user logs and event logs
-cp raw/*user*.log processed/user_logs
-cp raw/*event*.log processed/event_logs
+cp raw/*user*.log processed/user_logs/
+cp raw/*event*.log processed/event_logs/
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm raw/*ipaddr*
 rm processed/user_logs/*ipaddr*
